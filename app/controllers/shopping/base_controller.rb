506791f9a0f6799618482f030f876894295c6353
Jorge Pardiñas <jorge.parbros@parbros.net>
2013-01-21 14:22:27 -0600

    diferentes tamaños para las diferentes resoluciones en el home

--- app/controllers/shopping/base_controller.rb
+++ app/controllers/shopping/base_controller.rb
@@ -46,8 +46,8 @@ class Shopping::BaseController < ApplicationController
   def find_or_create_order
     return @session_order if @session_order
     if session[:order_id]
-      @session_order = current_user.orders.includes([ {:ship_address => :state},
-                                                      {:bill_address => :state},
+      @session_order = current_user.orders.includes([ {:ship_address},
+                                                      {:bill_address},
                                                       {:order_items =>
                                                         {:variant =>
                                                           {:product => :images }}}]).find(session[:order_id])
