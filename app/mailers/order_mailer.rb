1ad6780d556db5482900e6118668f902f20f3e2d
Jorge Pardiñas <jorge.parbros@parbros.net>
2013-03-07 22:28:44 -0600

    arreglando typo

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -3,6 +3,6 @@ class OrderMailer < ActionMailer::Base
   
   def order_confirmation(order)
     @order = order
-    mail(:bbc => 'jorge.sotomoreno@gmail.com', :to => [order.email], :subject => "Magnolia Cream Parlos - Confirmacion de orden")
+    mail(:bcc => ['jorge.sotomoreno@gmail.com','jparbros@gmail.com'], :to => [order.email], :subject => "Magnolia Cream Parlos - Confirmacion de orden")
   end
 end
